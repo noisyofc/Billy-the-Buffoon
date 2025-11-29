13406d9047659ccc31257f13e8b0e1c3b522e1d2
Pawel <pawel.nawrocki1994@gmail.com>
2025-11-29 22:15:30 +0100

    KameraNaWallrun

--- Assets/CODE/Scripts/Player Scripts/PlayerCam.cs
+++ Assets/CODE/Scripts/Player Scripts/PlayerCam.cs
@@ -19,8 +19,8 @@ public class PlayerCam : MonoBehaviour
     public Transform camHolder;    // Holder for the camera rotation
 
     // Internal rotation tracking
-    private float xRotation;
-    private float yRotation;
+    public float xRotation;
+    public float yRotation;
 
     public float yaw, pitch;
 
@@ -29,6 +29,13 @@ public class PlayerCam : MonoBehaviour
     public PlayerRespawnManager playerRespawnManager;
 
     private float tiltZ = 0f; // Add this at the top of your class
+
+    public bool cameraLocked = false;
+    public Vector2 lockedRotation;
+    [Header("Wallrun Camera Limits")]
+    public float maxYawOffset = 10f;   // maksymalny ruch w poziomie
+    public float maxPitchOffset = 5f;  // maksymalny ruch w pionie
+    private Vector2 wallrunLockedRotation; // kierunek centralny podczas wallrunu
 
     private void Start()
     {
@@ -43,61 +50,81 @@ public class PlayerCam : MonoBehaviour
 
     private void Update()
     {
-        mouseSensitivity = PlayerPrefs.GetFloat("MouseSensitivity", 1.0f);
-        PadSensitivity = PlayerPrefs.GetFloat("PadSensitivity", 0.5f);
-
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX * mouseSensitivity;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY * mouseSensitivity;
-        float PadX = Input.GetAxisRaw("Pad X") * Time.deltaTime * sensX * PadSensitivity;
-        float PadY = Input.GetAxisRaw("Pad Y") * Time.deltaTime * sensY * PadSensitivity;
-
-        yRotation += mouseX;
-        yRotation += PadX;
-        xRotation -= mouseY;
-        xRotation -= PadY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        // Apply the rotations to the camera holder and the player's orientation, including tilt
-        camHolder.rotation = Quaternion.Euler(xRotation, yRotation, tiltZ);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
-
-        if (playerRespawnManager.respawn == true)
+        if (cameraLocked)
         {
-            xRotation = yaw;
-            yRotation = pitch;
-            camHolder.rotation = Quaternion.Euler(xRotation, yRotation, tiltZ);
+            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX * mouseSensitivity;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY * mouseSensitivity;
+
+            yRotation += mouseX;
+            xRotation -= mouseY;
+
+            // Ograniczenie ruchu wzglêdem centralnego punktu
+            xRotation = Mathf.Clamp(xRotation, wallrunLockedRotation.x - maxPitchOffset, wallrunLockedRotation.x + maxPitchOffset);
+            yRotation = Mathf.Clamp(yRotation, wallrunLockedRotation.y - maxYawOffset, wallrunLockedRotation.y + maxYawOffset);
+
+            camHolder.localRotation = Quaternion.Euler(xRotation, yRotation, tiltZ);
             orientation.rotation = Quaternion.Euler(0, yRotation, 0);
-            if (playerRespawnManager.lastResp == "Res1")
+            return;
+        }
+        else
+        {
+            mouseSensitivity = PlayerPrefs.GetFloat("MouseSensitivity", 1.0f);
+            PadSensitivity = PlayerPrefs.GetFloat("PadSensitivity", 0.5f);
+
+            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX * mouseSensitivity;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY * mouseSensitivity;
+            float PadX = Input.GetAxisRaw("Pad X") * Time.deltaTime * sensX * PadSensitivity;
+            float PadY = Input.GetAxisRaw("Pad Y") * Time.deltaTime * sensY * PadSensitivity;
+
+            yRotation += mouseX;
+            yRotation += PadX;
+            xRotation -= mouseY;
+            xRotation -= PadY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            // Apply the rotations to the camera holder and the player's orientation, including tilt
+            //   camHolder.rotation = Quaternion.Euler(xRotation, yRotation, tiltZ);
+            camHolder.localRotation = Quaternion.Euler(xRotation, yRotation, tiltZ);
+            orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+            if (playerRespawnManager.respawn == true)
             {
-                camHolder.rotation = Quaternion.Euler(playerRespawnManager.respawnPoints[1].gameObject.transform.rotation.x, playerRespawnManager.respawnPoints[1].gameObject.transform.rotation.y, 0);
-                orientation.rotation = Quaternion.Euler(0, playerRespawnManager.respawnPoints[1].gameObject.transform.rotation.y, 0);
+                xRotation = yaw;
+                yRotation = pitch;
+                camHolder.rotation = Quaternion.Euler(xRotation, yRotation, tiltZ);
+                orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+                if (playerRespawnManager.lastResp == "Res1")
+                {
+                    camHolder.rotation = Quaternion.Euler(playerRespawnManager.respawnPoints[1].gameObject.transform.rotation.x, playerRespawnManager.respawnPoints[1].gameObject.transform.rotation.y, 0);
+                    orientation.rotation = Quaternion.Euler(0, playerRespawnManager.respawnPoints[1].gameObject.transform.rotation.y, 0);
+                }
+                if (playerRespawnManager.lastResp == "Res2")
+                {
+                    camHolder.rotation = Quaternion.Euler(playerRespawnManager.respawnPoints[2].gameObject.transform.rotation.x, playerRespawnManager.respawnPoints[2].gameObject.transform.rotation.y, 0);
+                    orientation.rotation = Quaternion.Euler(0, playerRespawnManager.respawnPoints[2].gameObject.transform.rotation.y, 0);
+                }
+                if (playerRespawnManager.lastResp == "Res3")
+                {
+                    camHolder.rotation = Quaternion.Euler(playerRespawnManager.respawnPoints[3].gameObject.transform.rotation.x, playerRespawnManager.respawnPoints[3].gameObject.transform.rotation.y, 0);
+                    orientation.rotation = Quaternion.Euler(0, playerRespawnManager.respawnPoints[3].gameObject.transform.rotation.y, 0);
+                }
+                if (playerRespawnManager.lastResp == "Res4")
+                {
+                    camHolder.rotation = Quaternion.Euler(playerRespawnManager.respawnPoints[4].gameObject.transform.rotation.x, playerRespawnManager.respawnPoints[4].gameObject.transform.rotation.y, 0);
+                    orientation.rotation = Quaternion.Euler(0, playerRespawnManager.respawnPoints[4].gameObject.transform.rotation.y, 0);
+                }
+                if (playerRespawnManager.lastResp == "Res5")
+                {
+                    camHolder.rotation = Quaternion.Euler(playerRespawnManager.respawnPoints[5].gameObject.transform.rotation.x, playerRespawnManager.respawnPoints[5].gameObject.transform.rotation.y, 0);
+                    orientation.rotation = Quaternion.Euler(0, playerRespawnManager.respawnPoints[5].gameObject.transform.rotation.y, 0);
+                }
+                if (playerRespawnManager.lastResp == "Res6")
+                {
+                    camHolder.rotation = Quaternion.Euler(playerRespawnManager.respawnPoints[6].gameObject.transform.rotation.x, playerRespawnManager.respawnPoints[6].gameObject.transform.rotation.y, 0);
+                    orientation.rotation = Quaternion.Euler(0, playerRespawnManager.respawnPoints[6].gameObject.transform.rotation.y, 0);
+                }
+                playerRespawnManager.respawn = false;
             }
-            if (playerRespawnManager.lastResp == "Res2")
-            {
-                camHolder.rotation = Quaternion.Euler(playerRespawnManager.respawnPoints[2].gameObject.transform.rotation.x, playerRespawnManager.respawnPoints[2].gameObject.transform.rotation.y, 0);
-                orientation.rotation = Quaternion.Euler(0, playerRespawnManager.respawnPoints[2].gameObject.transform.rotation.y, 0);
-            }
-            if (playerRespawnManager.lastResp == "Res3")
-            {
-                camHolder.rotation = Quaternion.Euler(playerRespawnManager.respawnPoints[3].gameObject.transform.rotation.x, playerRespawnManager.respawnPoints[3].gameObject.transform.rotation.y, 0);
-                orientation.rotation = Quaternion.Euler(0, playerRespawnManager.respawnPoints[3].gameObject.transform.rotation.y, 0);
-            }
-            if (playerRespawnManager.lastResp == "Res4")
-            {
-                camHolder.rotation = Quaternion.Euler(playerRespawnManager.respawnPoints[4].gameObject.transform.rotation.x, playerRespawnManager.respawnPoints[4].gameObject.transform.rotation.y, 0);
-                orientation.rotation = Quaternion.Euler(0, playerRespawnManager.respawnPoints[4].gameObject.transform.rotation.y, 0);
-            }
-            if (playerRespawnManager.lastResp == "Res5")
-            {
-                camHolder.rotation = Quaternion.Euler(playerRespawnManager.respawnPoints[5].gameObject.transform.rotation.x, playerRespawnManager.respawnPoints[5].gameObject.transform.rotation.y, 0);
-                orientation.rotation = Quaternion.Euler(0, playerRespawnManager.respawnPoints[5].gameObject.transform.rotation.y, 0);
-            }
-            if (playerRespawnManager.lastResp == "Res6")
-            {
-                camHolder.rotation = Quaternion.Euler(playerRespawnManager.respawnPoints[6].gameObject.transform.rotation.x, playerRespawnManager.respawnPoints[6].gameObject.transform.rotation.y, 0);
-                orientation.rotation = Quaternion.Euler(0, playerRespawnManager.respawnPoints[6].gameObject.transform.rotation.y, 0);
-            }
-            playerRespawnManager.respawn = false;   
         }
     }
 
@@ -118,6 +145,17 @@ public class PlayerCam : MonoBehaviour
     {
         tiltZ = zTilt;
         // Optionally keep the DOTween for smoothness, but always update tiltZ
-        camHolder.DOLocalRotate(new Vector3(xRotation, yRotation, tiltZ), 0.25f);
+      //  camHolder.DOLocalRotate(new Vector3(xRotation, yRotation, tiltZ), 0.25f);
+    }
+
+    public void LockCamera(Vector2 centerRotation)
+    {
+        wallrunLockedRotation = centerRotation;
+        cameraLocked = true;
+    }
+
+    public void UnlockCamera()
+    {
+        cameraLocked = false;
     }
 }
--- Assets/CODE/Scripts/Player Scripts/WallRunningAdvanced.cs
+++ Assets/CODE/Scripts/Player Scripts/WallRunningAdvanced.cs
@@ -176,6 +176,8 @@ public class WallRunningAdvanced : MonoBehaviour
                 cam.DoFov(90f);
                 if (wallLeft) cam.DoTilt(-5f);
                 if (wallRight) cam.DoTilt(5f);
+
+                cam.LockCamera(new Vector2(cam.xRotation, cam.yRotation));
             }
         }
     }
@@ -232,6 +234,7 @@ public class WallRunningAdvanced : MonoBehaviour
         // Reset camera effects
         cam.DoFov(80f);
         cam.DoTilt(0f);
+        cam.UnlockCamera();
     }
 
     /// <summary>
